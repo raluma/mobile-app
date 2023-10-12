9f1450fed901897192fd3cbcf35c0158e69371f4
Raúl Marrero Quevedo <ramaque01@gmail.com>
2023-10-12 11:54:14 +0100

    trying components

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Gif from './components/Gif';
+import Fav from "./components/Fav";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Gif fav={false}>
+        <Fav></Fav>
+      </Gif>
     </View>
   );
 }
