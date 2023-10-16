96257024c078659b000f65a7e29d9b7b0786b06c
Raúl Marrero Quevedo <ramaque01@gmail.com>
2023-10-16 17:17:49 +0100

    Creating the navigation

--- App.js
+++ App.js
@@ -1,25 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import FavBtn from './components/FavBtn';
-import Fav from "./components/Fav";
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import HomeScreen from "./pages/HomeScreen";
+import SettingsScreen from "./pages/SettingsScreen";
+import { Icon } from '@rneui/themed';
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-      <FavBtn>
-        <Fav></Fav>
-      </FavBtn>
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: () => {
+            return <Icon name={ route.name === "Home" ? "home" : "gear" } type="font-awesome" color="black" />;
+          },
+          tabBarActiveTintColor: 'black',
+          tabBarInactiveTintColor: 'black',
+        })}
+      >
+        <Tab.Screen 
+          name="Home" 
+          component={HomeScreen} 
+        />
+
+        <Tab.Screen 
+          name="Settings" 
+          component={SettingsScreen} 
+        />
+
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
