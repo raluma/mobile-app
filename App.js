70b22caa220a8a1af8250d5b24028678910a7f9e
Raúl Marrero Quevedo <ramaque01@gmail.com>
2023-10-23 19:49:57 +0100

    changing project routes

---
+++ App.js
@@ -0,0 +1,46 @@
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import HomeScreen from "./src/pages/HomeScreen";
+import FavsScreen from "./src/pages/FavsScreen";
+import SettingsScreen from "./src/pages/SettingsScreen";
+import { Icon } from '@rneui/themed';
+
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: () => {
+            return <Icon name={ 
+              route.name === "Home" ? "home" 
+              : route.name === "Favs" ? "heart"
+              : "gear" } type="font-awesome" color="black" />;
+          },
+          tabBarActiveTintColor: 'black',
+          tabBarInactiveTintColor: 'black',
+        })}
+      >
+        <Tab.Screen 
+          name="Home" 
+          component={HomeScreen}
+        />
+
+        <Tab.Screen 
+          name="Favs" 
+          component={FavsScreen}
+          options={{ tabBarBadge: "!" }}
+        />
+
+        <Tab.Screen 
+          name="Settings" 
+          component={SettingsScreen}
+          options={{ tabBarBadge: "!" }} 
+        />
+
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
