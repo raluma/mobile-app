7501a055b8708ba331ed79531848aabc58f0a261
Raúl Marrero Quevedo <ramaque01@gmail.com>
2023-10-26 18:19:29 +0100

    creating a signup component (style)

---
+++ src/components/Signup.js
@@ -0,0 +1,49 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, View } from 'react-native';
+import { TextInput, Button } from 'react-native-paper';
+
+export default function Login() {
+
+  return (
+    <View style={styles.container}>
+      <TextInput
+        label="Username"
+        style={styles.textInput}
+      />
+
+      <TextInput
+        label="Password"
+        secureTextEntry
+        right={<TextInput.Icon icon="eye" />}
+        style={styles.textInput}
+      />
+
+      <Button 
+        mode="elevated" 
+        onPress={() => console.log('Pressed')}
+        style = {styles.button}
+        >
+          Signup
+      </Button>
+
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: "#fff",
+      alignItems: "center",
+      justifyContent: "center",
+      gap: 26
+    },
+    textInput: {
+      width: 260
+    }, 
+    button: {
+      width: 160,
+      borderRadius: 2
+    }
+});
