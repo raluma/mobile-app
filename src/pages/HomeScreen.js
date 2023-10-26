a67c9fa84f4375a2b12181d8e3a73363b6fed2fa
Raúl Marrero Quevedo <ramaque01@gmail.com>
2023-10-26 22:19:23 +0100

    cleaning

--- src/pages/HomeScreen.js
+++ src/pages/HomeScreen.js
@@ -1,13 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import GifsStack from '../components/GifsStack';
 
-export default function HomeScreen() {
-    return (
-      <>
-        <GifsStack limit={6} />
-        <StatusBar style="auto" />
-      </>
-    );
+
+export default function HomeScreen({ loged, setLoged }) {
+  return (
+    <>
+
+    </>
+  );
 }
 
 
