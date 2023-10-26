e88fab9d712a64be9c34b20c642ce4d294cc60ce
Raúl Marrero Quevedo <ramaque01@gmail.com>
2023-10-26 18:18:26 +0100

    creating a stack of gifs

--- src/components/GifsStack.js
+++ src/components/GifsStack.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Image } from 'react-native';
+import { View, Image, StyleSheet } from 'react-native';
 import { useTrendingGifs } from '../hooks/useTrendingGifs';
 import Gif from './Gif';
 
@@ -7,21 +7,45 @@ export default function GifsStack({ limit }) {
   const trendingGifs = useTrendingGifs({ limit });
 
   return (
-    <View>
-      {trendingGifs.loading === true ?
-        <Image
-          source={{ uri: "https://media0.giphy.com/media/3oEjI6SIIHBdRxXI40/giphy.gif?cid=ecf05e47pv652litdm9zcy95wzorvw3qwd0dcxegxfzhb3yw&ep=v1_gifs_search&rid=giphy.gif&ct=g" }}
-          style={{ width: 100, height: 100 }}
-        />
-        :
-        trendingGifs.results.map(gif => {
-          <Gif
-            uri={gif.url}
-            width={200}
-            height={200}
+    <>
+      {trendingGifs.loading ?
+        <View style={styles.containerLoader}>
+          <Image
+            source={{ uri: "https://media0.giphy.com/media/3oEjI6SIIHBdRxXI40/giphy.gif?cid=ecf05e47pv652litdm9zcy95wzorvw3qwd0dcxegxfzhb3yw&ep=v1_gifs_search&rid=giphy.gif&ct=g" }}
+            style={{ width: 100, height: 100 }}
           />
-        })
+        </View>
+        :
+        <View style={styles.containerGifs}>
+          {
+            trendingGifs.results.map(gif => {
+              return <Gif key={gif.id}
+                uri={ gif.url }
+                width= { 150 }
+                height= { 150 }
+              />
+            })
+          }
+        </View>
       }
-    </View>
+    </>
   );
 }
+
+const styles = StyleSheet.create({
+  containerLoader: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  containerGifs: {
+    flex: 1,
+    flexDirection: "row",
+    flexWrap: "wrap",
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    gap: 30,
+  },
+});
