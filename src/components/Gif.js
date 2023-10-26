4a2077926b01307ac28bb6a262e0fcc3e01942a4
Raúl Marrero Quevedo <ramaque01@gmail.com>
2023-10-26 18:18:07 +0100

    creating gif component

--- src/components/Gif.js
+++ src/components/Gif.js
@@ -1,17 +1,21 @@
 import React from 'react';
-import { Image } from 'react-native';
+import { View, Image } from 'react-native';
 import FavBtn from './FavBtn'
 import Fav from './Fav';
 
 export default function Gif({ uri, width, height }) {
 
     return (
-        <Image 
-            source = {{ uri }}  
-            style = {{ width, height }} 
-        />
-        // <FavBtn>
-        //   <Fav />
-        // </FavBtn>
+        <View>
+            <Image 
+                source = {{ uri }}  
+                style = {{ width, height }} 
+            />
+
+            <FavBtn>
+                <Fav />
+            </FavBtn>
+        </View>
+        
     );
 }
