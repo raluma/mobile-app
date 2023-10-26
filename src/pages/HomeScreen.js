0d5e08c60bc09543d9ca4849fee1febb409240eb
Raúl Marrero Quevedo <ramaque01@gmail.com>
2023-10-26 18:20:45 +0100

    refactoring home screen

--- src/pages/HomeScreen.js
+++ src/pages/HomeScreen.js
@@ -1,22 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View } from 'react-native';
 import GifsStack from '../components/GifsStack';
 
 export default function HomeScreen() {
     return (
-      <View style={styles.container}>
-        <GifsStack limit={2} />
+      <>
+        <GifsStack limit={6} />
         <StatusBar style="auto" />
-      </View>
+      </>
     );
 }
 
-const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-});
 
