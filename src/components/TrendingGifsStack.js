cf1ea64c71eb2fd9e33e011f7dcfcb00af6f63b4
Raúl Marrero Quevedo <ramaque01@gmail.com>
2023-10-27 23:21:48 +0100

    adding logout

--- src/components/TrendingGifsStack.js
+++ src/components/TrendingGifsStack.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
 import { useTrendingGifs } from '../hooks/useTrendingGifs';
+import { Button } from 'react-native-paper';
+import { logOut } from '../services/accountFunctions';
 import Loader from './Loader';
 import Gif from './Gif';
 
-export default function TrendingGifsStack({ limit }) {
+export default function TrendingGifsStack({ setLoged, limit }) {
   const trendingGifs = useTrendingGifs({ limit });
+
+  const onPress = () => {
+    logOut();
+    setLoged({loading: false, result: false});
+  }
 
   return (
     <>
@@ -13,16 +20,26 @@ export default function TrendingGifsStack({ limit }) {
         <Loader />
         :
         <View style={styles.container}>
-          {
-            trendingGifs.results.map(gif => {
-              return <Gif key={gif.id}
-                uri={ gif.url }
-                width= { 150 }
-                height= { 150 }
-                optionFav={ false }
-              />
-            })
-          }
+          <Button 
+              mode="elevated" 
+              onPress={onPress}
+              style={styles.button}
+          >
+            Logout
+          </Button>
+
+          <View style={styles.containerGifs}>
+            {
+              trendingGifs.results.map(gif => {
+                return <Gif key = { gif.id }
+                  uri ={ gif.url }
+                  width = { 150 }
+                  height = { 150 }
+                  optionFav = { false }
+                />
+              })
+            }
+          </View>
         </View>
       }
     </>
@@ -32,6 +49,15 @@ export default function TrendingGifsStack({ limit }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: "column",
+    flexWrap: "wrap",
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    gap: 30,
+  },
+  containerGifs: {
+    flex: 1,
     flexDirection: "row",
     flexWrap: "wrap",
     backgroundColor: '#fff',
@@ -39,4 +65,8 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     gap: 30,
   },
+  button: {
+    width: 160,
+    borderRadius: 2
+  }
 });
